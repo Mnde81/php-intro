5ffeb1909ff759ad99c3b0940dfc68c4f2b8810d
Mnde81 <minubis@gmail.com>
2024-10-09 08:01:35 +0300

    updated file

---
+++ comments.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+   <?php
+    
+    /* This function below displays text
+    
+    This is the second line */
+    
+    
+    // One line commnent
+        
+    echo "Hello Student";
+    
+    
+    
+    ?>
+    
+</body>
+</html>
