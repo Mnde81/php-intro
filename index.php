c6f1da878d8002e86934ded4c523e0f6955d93f4
Mnde81 <minubis@gmail.com>
2024-10-14 07:57:55 +0300

    updated file

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-hello
+<?php
+
+
+// ini_set('display_errors', 1);
+
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+
+// phpinfo();
+
+error_log('whatever message we want', 3, 'somefile.log');
+
+require('contact.php');
