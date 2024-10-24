9ec517fd19b7b245cecb7153f8cabe0b3b71e755
Mnde81 <minubis@gmail.com>
2024-10-24 10:42:09 +0300

    updated file

---
+++ validation/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validation</title>
+</head>
+<body>
+
+<form action="process.php" method="post">
+    <input name="username" type="text" placeholder="Enter Username">
+    <input name="email" type="text" placeholder="Enter Email"><br><br>
+    <input type="submit" value="Submit">
+</form>
+    
+</body>
+</html>
